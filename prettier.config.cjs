518b9789f341e5eb7d19b07dc1d6735cc0606a96
Ian Kusner <ikusner13@gmail.com>
2023-04-28 22:17:42 -0400

    setup stuff

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  arrowParens: "always",
+  printWidth: 80,
+  jsxSingleQuote: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
