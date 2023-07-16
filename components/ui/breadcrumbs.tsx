fd26ea96e2495c9bd53e1a2abecde210b11b047d
jhamilton14 <jshamilton1414@gmail.com>
2023-07-16 19:59:09 -0400

    breadcrumbs

--- components/ui/breadcrumbs.tsx
+++ components/ui/breadcrumbs.tsx
@@ -4,54 +4,61 @@ import * as React from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+import { GUNS_TO_WEAPONS } from "@/config/weapons"
+
 const Breadcrumbs = () => {
   const pathname = usePathname()
 
   const pathArray = pathname.split("/").filter((x) => x)
 
-  if (pathArray.length === 1) return null
+  if (pathArray.length === 0) return null
 
-  const correctPathArray = pathArray.map((path, index) => {
-    const decodedPath = decodeURIComponent(path)
-    if (decodedPath === "skin") {
-      return {
-        path: "/weapons",
-        name: "weapons",
-      }
-    }
+  const correctPathArray = [
+    {
+      path: "/",
+      name: "home",
+    },
+  ]
 
-    if (index === 1 && pathArray.length === 3) {
-      return {
-        path: `/weapons/${decodedPath}`,
-        name: decodedPath,
-      }
-    }
+  if (pathArray.includes("skin") && pathArray.length === 3) {
+    const gunName = pathArray[1]
+    const weapon = GUNS_TO_WEAPONS[gunName]
 
-    if (index === 2 && pathArray.length === 3) {
-      return {
+    correctPathArray.push(
+      {
+        path: `/${weapon}`,
+        name: weapon,
+      },
+      {
+        path: `/${weapon}/${gunName}`,
+        name: gunName,
+      },
+      {
         path: "",
-        name: decodedPath,
+        name: decodeURI(pathArray[2]),
       }
-    }
-
-    return {
-      path: `/${decodedPath}`,
-      name: decodedPath,
-    }
-  })
+    )
+  } else {
+    pathArray.forEach((path) => {
+      correctPathArray.push({
+        path: `/${path}`,
+        name: path,
+      })
+    })
+  }
 
   return (
     <nav className="flex flex-wrap text-sm text-foreground">
       {correctPathArray.map((path, index) => {
         return (
           <div key={index} className="flex items-center">
-            {index === pathArray.length - 1 ? (
+            {index === correctPathArray.length - 1 ? (
               <span className="text-accent">{path.name}</span>
             ) : (
               <Link href={path.path}>{path.name}</Link>
             )}
 
-            {index !== pathArray.length - 1 && (
+            {index !== correctPathArray.length - 1 && (
               <span className="mx-2"> &gt; </span>
             )}
           </div>
