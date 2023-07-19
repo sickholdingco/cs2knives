abf3232a41226cf9bdeb561185a768e5a2df83a6
jhamilton14 <jshamilton1414@gmail.com>
2023-07-18 22:30:44 -0400

    removed dead code

--- components/ui/breadcrumbs.tsx
+++ components/ui/breadcrumbs.tsx
@@ -14,46 +14,6 @@ interface BreadcrumbsProps {
 }
 
 const Breadcrumbs = ({ crumbs }: BreadcrumbsProps) => {
-  // const pathname = usePathname()
-
-  // const pathArray = pathname.split("/").filter((x) => x)
-
-  // if (pathArray.length === 0) return null
-
-  // const correctPathArray = [
-  //   {
-  //     path: "/",
-  //     name: "home",
-  //   },
-  // ]
-
-  // if (crumbs) {
-  //   const gunName = pathArray[1]
-  //   const weapon = GUNS_TO_WEAPONS[gunName]
-
-  //   correctPathArray.push(
-  //     {
-  //       path: `/${weapon}`,
-  //       name: weapon,
-  //     },
-  //     {
-  //       path: `/${weapon}/${gunName}`,
-  //       name: decodeURI(gunName),
-  //     },
-  //     {
-  //       path: "",
-  //       name: decodeURI(pathArray[2]),
-  //     }
-  //   )
-  // } else {
-  //   pathArray.forEach((path) => {
-  //     correctPathArray.push({
-  //       path: `/${path}`,
-  //       name: decodeURI(path),
-  //     })
-  //   })
-  // }
-
   return (
     <nav className="flex flex-wrap text-sm text-foreground">
       {crumbs.map((crumb, index) => {
